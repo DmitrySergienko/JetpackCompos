ed516475162604f2efab4afb4263cb8ab90f384b
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-09 08:55:10 +0400

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposcardviewl2/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.jetpackcomposcardviewl2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposcardviewl2.ui.theme.JetpackComposCardViewL2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column() {
+
+                NewCardView("Name", "Actor")
+                NewCardView("Name", "Actor")
+                NewCardView("Name", "Actor")
+                NewCardView("Name", "Actor")
+                NewCardView("Name", "Actor")
+            }
+
+        }
+    }
+}
+
+@Composable
+fun NewCardView(name: String, prof: String) {
+
+    Card(
+        modifier = Modifier
+
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box() {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_pic_currency_back),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(CircleShape)
+                        .padding(5.dp)
+                )
+                Column(modifier = Modifier.padding(start = 10.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
+    }
+
+
+}
