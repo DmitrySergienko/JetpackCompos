177d5a4758d5e6f82dc9a58a98d89a67d5b7bd74
DmitrySergienko <dmitrisergienko@gmail.com>
2022-06-09 10:19:34 +0400

    5. Modifier.pointerInput()

--- app/src/main/java/com/example/jetpackcomposcardviewl2/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposcardviewl2/MainActivity.kt
@@ -2,27 +2,26 @@ package com.example.jetpackcomposcardviewl2
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpackcomposcardviewl2.ui.theme.JetpackComposCardViewL2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,8 +54,13 @@ fun NewCardView(name: String, prof: String) {
             .padding(10.dp)
             .offset(30.dp)
             .clickable {
-                Log.d("Test", "Clickable test")
+                Log.d("VVV", "Clickable test")
 
+            }
+            .pointerInput(Unit) {
+                detectDragGesturesAfterLongPress { change, dragAmount ->
+                    Log.d("VVV", "$dragAmount")
+                }
             },
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
